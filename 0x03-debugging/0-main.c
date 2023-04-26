f79707a5923f897f0290c869eb24a1fc53e638d8
maureen-chepr <maureenchep95@gmail.com>
2023-04-26 09:55:41 -0700

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -12,17 +12,7 @@ int main(void)
 	int i;
 
 	i = 0;
-	if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else
-	{
-		printf("%d is negative\n", i);
-	}
-	return (0);
+	positive_or_negative(i);
+
+        return (0);
 }
