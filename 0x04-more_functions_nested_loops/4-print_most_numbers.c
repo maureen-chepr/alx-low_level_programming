60a3cd322e890b71629cdaeda5cf5c41882edcf1
maureen-chepr <maureenchep95@gmail.com>
2023-04-27 07:50:36 -0700

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,11 @@ void print_most_numbers(void)
 {
 	int c = 48;
 
-	for ( c = 48; c <= 57; c++ )
+	for (c = 48; c <= 57; c++)
 	{
 		if ((c != 50) && (c != 52))
 		{
 			_putchar (c);
-			
 		}
 	}
 	_putchar ('\n');
