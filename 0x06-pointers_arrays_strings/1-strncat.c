97113a94ca813e0043fc7a9f7920a6c07f55184f
maureen-chepr <maureenchep95@gmail.com>
2023-05-04 05:29:59 -0700

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,18 +5,18 @@
  * @dest: first string
  * @src: second string
  * @n: number of bytes
- * Return: 0 
+ * Return: 0
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-        int i, length1;
-	length1 = strlen(dest);
+	int i, length1;
 
-        for (i = 0; i < n && src[i] != '\0'; i++)
-        {
+	length1 = strlen(dest);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
 		dest[length1 + i] = src[i];
 	}
-	src[length1 + i] = '\0';
-        return (dest);
+	dest[length1 + i] = '\0';
+	return (dest);
 }
