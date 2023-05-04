3bb848bdba7af3dc4a4b6a6ab8749c8482e6b6a5
maureen-chepr <maureenchep95@gmail.com>
2023-05-04 08:28:19 -0700

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - function that compares two strings
+ * @*s1: first string
+ * @*s2: second string
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+
