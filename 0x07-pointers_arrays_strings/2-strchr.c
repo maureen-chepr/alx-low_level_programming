fe38cb36f7bfa7dc15f4e3bb6be77ad7ed0bd39b
maureen-chepr <maureenchep95@gmail.com>
2023-05-08 02:52:21 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -16,7 +16,7 @@ char *_strchr(char *s, char c)
 	{
 		if (s[idx] == c)
 		{
-			return &s[idx];
+			return (&s[idx]);
 		}
 	}
 	return (NULL);
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints the chessboard.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for ( j = 0; j < 8; j++)
+		{
+			_putchar
