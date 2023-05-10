7ec584e246073598f959110557876a88099d96c0
maureen-chepr <maureenchep95@gmail.com>
2023-05-10 04:34:51 -0700

    101-wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,7 +16,7 @@ int wildcmp(char *s1, char *s2)
 	}
 	if (*s1 == *s2)
 	{
-		return (wildcmp(s1 +1, s2 + 1));
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	if (*s2 == '*')
 	{
@@ -24,7 +24,7 @@ int wildcmp(char *s1, char *s2)
 		{
 			return (1);
 		}
-		if (*s1 != '\0' && wildcmp(s1 +1, s2))
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
 		{
 			return (1);
 		}
