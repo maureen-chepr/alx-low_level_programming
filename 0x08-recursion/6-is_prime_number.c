ec6f2fe366e61c1cfd16ecfa341dc8d7b44a4146
maureen-chepr <maureenchep95@gmail.com>
2023-05-10 03:15:57 -0700

    6-is_prime_number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,26 +1,30 @@
 #include "main.h"
 
 /**
- * is_prime_number - function that returns 1 if the input
- * integer is a prime number, otherwise return 0
- * @n: integer to be checked
- * @i: checks if n is prime number
- * Return: 0
+ * is_prime_number - says if an integer is a prime number or not
+ * @n: number to evaluate
+ *
+ * Return: 1 if n is a prime number, 0 if not
  */
-
 int is_prime_number(int n)
 {
+	if (n <= 1)
+		return (0);
+	return (actual_prime_number(n, n - 1));
+}
 
-	if (n == 1)
-	{
+/**
+ * actual_prime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int actual_prime_number(int n, int i)
+{
+	if (i == 1)
 		return (1);
-	}
-	else if (n % n == 0)
-	{
-		return (1);
-	}
-	else
-	{
-		return (is_prime_number(n, n + 1));
-        }
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime_number(n, i - 1));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,4 +12,5 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int real_sqrt_recursion(int n, int i);
+int actual_prime_number(int n, int i);
 #endif
