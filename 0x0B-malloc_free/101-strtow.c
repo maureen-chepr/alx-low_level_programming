fbeb8f321f36d5a6058b6d8bca56b645881848a5
maureen-chepr <maureenchep95@gmail.com>
2023-05-16 11:38:24 -0700

    101-strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,23 +1,18 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * strtow - function that splits a string into words
  * @str: string to be split into words
- * Return: pointer to an array of strings (Success)
- * or NULL (fail)
+ * Return: pointer or NULL (fail)
  */
 char **strtow(char *str)
 {
-	int words = 0;
-	int x, y, z;
+	int words = 0, x, y, z;
 	char **array;
 
 	if (str == NULL || *str == '\0')
-	{
 		return (NULL);
-	}
 	for (x = 0; str[x] != '\0'; x++)
 	{
 		if (str[x] != ' ' && (x == 0 || str[x - 1] == ' '))
@@ -25,11 +20,8 @@ char **strtow(char *str)
 	}
 	array = (char **)malloc((words + 1) * sizeof(char *));
 	if (array == NULL)
-	{
 		return (NULL);
-	}
-	x = 0;
-	y = 0;
+	x = 0, y = 0;
 	while (str[x] != '\0')
 	{
 		if (str[x] != ' ')
@@ -37,19 +29,15 @@ char **strtow(char *str)
 			int lgth = 0;
 
 			z = x;
-
 			while (str[z] != ' ' && str[z] != '\0')
 			{
-				lgth++;
-				z++;
+				lgth++, z++;
 			}
 			array[y] = (char *)malloc((lgth + 1) * sizeof(char));
 			if (array[y] == NULL)
 			{
 				while (y > 0)
-					free(array[--y]);
-				free(array);
-				return (NULL);
+					return (NULL);
 			}
 			for (z = 0; z < lgth; z++, x++)
 				array[y][z] = str[x];
@@ -57,10 +45,7 @@ char **strtow(char *str)
 			y++;
 		}
 		else
-		{
 			x++;
-		}
 	}
-	array[y] = NULL;
 	return (array);
 }
