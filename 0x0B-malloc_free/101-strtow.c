72cf6f3fa1c3c8f4564a3344d6c22afc675f10dc
maureen-chepr <maureenchep95@gmail.com>
2023-05-16 07:41:34 -0700

    2-str_concat

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,7 +9,7 @@
  */
 int word_count(char *str)
 {
-	int flag, x, y = 0;
+	int flag = 0, x, y = 0;
 
 	for (x = 0; str[x] != '\0'; x++)
 	{
@@ -32,46 +32,52 @@ int word_count(char *str)
  */
 char **strtow(char *str)
 {
-	char **array, *s1;
-	int x, y = 0, lgth = 0, words, z = 0, start, end;
+	char **array;
+	int words, idx;
+	int len, start, word_len, i, j;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-
-	lgth = strlen(str);
 	words = word_count(str);
 	if (words == 0)
-	{
 		return (NULL);
-	}
-	array = (char **)malloc((words + 1) * sizeof(char *));
+	array = malloc((words + 1) * sizeof(char *));
 	if (array == NULL)
-	{
 		return (NULL);
-	}
-	for (x = 0; x <= lgth; x++)
+	len = strlen(str);
+	idx = 0;
+	start = 0;
+	word_len = 0;
+
+	for (i = 0; i <= len; i++)
 	{
-		if (str[x] == ' ' || str[x] == '\0')
+		if (str[i] == ' ' || str[i] == '\0')
 		{
-			if (z)
+			if (word_len > 0)
 			{
-				end = x;
-				s1 = (char *)malloc((z + 1) * sizeof(char));
-				if (s1 == NULL)
+				array[idx] = malloc((word_len + 1) * sizeof(char));
+				if (array[idx] == NULL)
+				{
+					for (j = 0; j < idx; j++)
+						free(array[j]);
+					free(array);
 					return (NULL);
-				while (start < end)
-					*s1++ = str[start++];
-				*s1= '\0';
-				array[y] = s1 - z;
-				y++;
-				z = 0;
-			}
-			else if (z++ == 0)
-			{
-				start = x;
+				}
+				strncpy(array[idx], &str[start], word_len);
+				array[idx][word_len] = '\0';
+				idx++;
+				word_len = 0;
 			}
 		}
+		else
+		{
+			if (word_len == 0)
+			{
+				start = i;
+			}
+			word_len++;
+		}
 	}
-	array[y] = NULL;
+	array[idx] = NULL;
 	return (array);
 }
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,13 +16,10 @@ char *str_concat(char *s1, char *s2)
 	unsigned int x;
 
 	if (s1 == NULL)
-	{
-		return ("");
-	}
+		s1 = "";
 	if (s2 == NULL)
-	{
-		return ("");
-	}
+		s2 = ("");
+	
 	str1 = strlen(s1);
 	str2 = strlen(s2);
 	size = (str1 + str2) + 1;
