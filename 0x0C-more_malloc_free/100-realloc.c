fb7e37058f8e25dc8c0c53cf11d3f52cb07eb1da
maureen-chepr <maureenchep95@gmail.com>
2023-05-18 06:17:12 -0700

    100-realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,8 +13,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	if (new_size == 0 && ptr != NULL)
 	{
-		return (NULL);
 		free(ptr);
+		return (NULL);
 	}
 	if (ptr == NULL)
 	{
@@ -24,6 +24,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (ptr);
 	}
+	free(ptr);
 	ptr = malloc(new_size);
 	return (ptr);
 }
