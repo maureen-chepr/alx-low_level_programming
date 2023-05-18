fee9942e29e4adbea1d86fd09a6e99475e310ae4
maureen-chepr <maureenchep95@gmail.com>
2023-05-18 08:07:33 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,28 @@
 #include "main.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
- *
+ * main - program that multiplies two positive numbers
+ * @argc:argument passed to main
+ * @argv:character array of pointers
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = num1 * num2;
+
+	printf("%d\n", mul);
+	return (0);
+}
