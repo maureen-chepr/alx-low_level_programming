cc48377c974c3f6ca8a09086b3bf049552ee78c6
maureen-chepr <maureenchep95@gmail.com>
2023-05-20 08:07:12 -0700

    101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,60 +1,39 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <ctype.h>
-
 /**
- * multiply - multiplies two positive integers
- * @num1: first number
- * @num2: second number
- * Return: the product of num1 and num2
+ * main - program that multiplies two positive numbers
+ * @argc:argument passed to main
+ * @argv:character array of pointers
+ * Return: 0
  */
-int multiply(int num1, int num2)
-{
-	return (num1 * num2);
-}
 
-/**
- * isPositiveNumber - checks if a string represents a positive number
- * @s: string to be evaluated
- * Return: 1 if s is a positive number, 0 otherwise
- */
-int isPositiveNumber(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		if (!isdigit(s[i]))
-			return (0);
-		i++;
-	}
-
-	return (1);
-}
-
-/**
- * main - multiplies two positive numbers
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 on success, 1 on error
- */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+	int num1;
+	int num2;
+	int mul, x, y;
 
-	if (argc != 3 || !isPositiveNumber(argv[1]) || !isPositiveNumber(argv[2]))
+	if (argc != 3)
 	{
 		printf("Error\n");
-		return (1);
+		exit(98);
 	}
-
+	for (x = 1; x < argc; x++)
+	{
+		for (y = 0; argv[x][y] != '\0'; y++)
+		{
+			if (!isdigit(argv[x][y]))
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
+	mul = num1 * num2;
 
-	result = multiply(num1, num2);
-
-	printf("%d\n", result);
-
+	printf("%d\n", mul);
 	return (0);
 }
-
