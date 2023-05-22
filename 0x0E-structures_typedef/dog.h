6980408dbd410a35961708983ca124bb4a4c004f
maureen-chepr <maureenchep95@gmail.com>
2023-05-22 02:26:14 -0700

    Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef STRUCTDOG_H
+#define STRUCTDOG_H
+/**
+ * struct dog - description
+ * @name: First type
+ * @age: Second type
+ * @owner: Third type
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+       	char *name;
+	float age;
+	char *owner;
+};
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
