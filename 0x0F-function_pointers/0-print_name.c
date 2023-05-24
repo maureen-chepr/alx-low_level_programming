bfae41bb2bc316a3d157308904028965010f3740
maureen-chepr <maureenchep95@gmail.com>
2023-05-24 03:12:04 -0700

    print_name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,10 +2,15 @@
 
 /**
  * print_name - function that prints a name.
- * @name: argument
+ * @name:argument
+ * @f:function pointer
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
 	f(name);
 }
