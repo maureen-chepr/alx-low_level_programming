b6268d2cc0fb01a1483f0cbf7335ecf2fd41fd9a
maureen-chepr <maureenchep95@gmail.com>
2023-05-25 02:30:34 -0700

    print_all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,53 +10,40 @@
 void print_all(const char * const format, ...)
 {
 	char *str;
-       	int i;
+	int i;
 	float f;
 	char c;
 	unsigned int idx = 0;
-	int flag = 0;
-
 	va_list lists;
-	va_start(lists, format);
 
+	va_start(lists, format);
 	while (format[idx])
 	{
 		if (format[idx] == 'c')
 		{
 			c = (char)va_arg(lists, int);
 			printf("%c", c);
-			flag = 1;
 		}
 		else if (format[idx] == 'i')
 		{
 			i = va_arg(lists, int);
 			printf("%d", i);
-			flag = 1;
 		}
 		else if (format[idx] == 'f')
 		{
 			f = (float)va_arg(lists, double);
 			printf("%f", f);
-			flag = 1;
 		}
 		else if (format[idx] == 's')
 		{
 			str = va_arg(lists, char *);
 			if (str == NULL)
-			{
 				printf("(nil)");
-			}
 			else
-			{
 				printf("%s", str);
-			}
-			flag = 1;
 		}
-		if (format[idx + 1] != '\0' && flag)
-		{
+		if (format[idx + 1] != '\0' && (format[idx] == 'c' || format[idx] == 'i' || format[idx] == 'f' || format[idx] == 's'))
 			printf(", ");
-			flag = 0;
-		}
 		idx++;
 	}
 	va_end(lists);
