f62011068ffef8b70e855d84b599d9c45b2e9e3d
maureen-chepr <maureenchep952gamil.com>
2023-06-08 04:27:22 -0700

    value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,6 +3,7 @@
 /**
  * get_bit -  function that returns the value of a bit at a given index
  * @index: the index, starting from 0 of the bit you want to get
+ * @n: binary number
  * Return: value of the bit at index index or -1 if an error occured
  */
 
