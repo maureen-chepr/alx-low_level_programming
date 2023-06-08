485082b824e224bca940509172d02d78fe0a1b9d
maureen-chepr <maureenchep952gamil.com>
2023-06-08 04:10:45 -0700

    binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,26 +7,26 @@
 
 void print_binary(unsigned long int n)
 {
-	 int ind = 63;
-	 int bits = 0;
-	 unsigned long int currbits;
-	 
-	 while (ind >= 0)
-	 {
-		 currbits = n >> ind;
-		 if (currbits & 1)
-		 {
-			 _putchar('1');
-			 bits++;
-		 }
-		 else if (bits)
-		 {
-			 _putchar('0');
-		 }
-		 ind--;
-	 }
-	 if (!bits)
-	 {
-		 _putchar('0');
-	 }
+	int ind = 63;
+	int bits = 0;
+	unsigned long int currbits;
+
+	while (ind >= 0)
+	{
+		currbits = n >> ind;
+		if (currbits & 1)
+		{
+			_putchar('1');
+			bits++;
+		}
+		else if (bits)
+		{
+			putchar('0');
+		}
+		ind--;
+	}
+	if (!bits)
+	{
+		_putchar('0');
+	}
 }
