00d2fca73e4d269248ebfc40b6a209a003a02e48
maureen-chepr <maureenchep95@gmail.com>
2023-10-16 03:07:16 -0700

    task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-wget -P /tmp https://https://github.com/maureen-chepr/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so 
+wget -P /tmp https://github.com/maureen-chepr/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
